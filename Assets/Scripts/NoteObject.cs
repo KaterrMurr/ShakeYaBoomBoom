6f953269bec3ac34c9ce81d0e57d6c269a5757dd
Tori Chistyakova <tori.chistiakova@gmail.com>
2023-01-05 21:09:58 +0300

    попытки-пытки

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -8,7 +8,7 @@ public class NoteObject : MonoBehaviour
     //и если можем, то была ли она нажата? 
     //Если была - добавляем очков игроку.
     
-    private bool _canBePressed;
+    public bool _canBePressed;
     
     // Start is called before the first frame update
     void Start()
@@ -19,7 +19,20 @@ public class NoteObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(raycast, out raycastHit))
+            {
+                Debug.Log("Something Hit");
+               
+                if (raycastHit.collider.CompareTag("Note"))
+                {
+                    Debug.Log("Note hit");
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
